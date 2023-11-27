d5f9a660a9f96064c9c81e588c35e37ecd6a3a81
Prasad Yeole <120306210+Prasad-Yeole@users.noreply.github.com>
2023-11-27 13:54:27 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,12 @@
+'''
+Description      :  Use Of Basic Input / Output  
+Function Author  :  Prasad Yeole
+Input            :  Str
+Output           :  --
+'''
+
+print("Enter One Number") # to display on screen
+no = input() # to accept the standard input device ie keyword
+
+print ("Your Number Is : ", no)
+print ("Data Type Of Give Number Is : ", type(no))
